0b2ab046ab58493e0387834752e9ff63106d45e7
Mohit <mohitcod141@gmail.com>
2025-05-26 16:40:07 +0530

    aggregation pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -298,7 +298,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Error While uploading  avatar");
   }
 
- const newAvatar =  await User.findByIdAndUpdate(
+  const newAvatar = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: { avatar: avatar.url },
@@ -306,12 +306,10 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     { new: true }
   ).select("-password");
 
-
-  return res.status(200)
-  .json(new ApiResponse(200 ,newAvatar , "Avatar file change successFully" ))
+  return res
+    .status(200)
+    .json(new ApiResponse(200, newAvatar, "Avatar file change successFully"));
 });
-
-
 
 const updateUserCoverImage = asyncHandler(async (req, res) => {
   const CoverImageLocalPath = req.file?.path;
@@ -325,7 +323,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Error While uploading  Cover Image");
   }
 
- const newCoverImage =  await User.findByIdAndUpdate(
+  const newCoverImage = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: { CoverImage: CoverImage.url },
@@ -333,81 +331,81 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     { new: true }
   ).select("-password");
 
-
-  return res.status(200)
-  .json(new ApiResponse(200 ,newCoverImage , "Cover Image  change successFully" ))
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, newCoverImage, "Cover Image  change successFully")
+    );
 });
 
-const getUserChannelDetails = asyncHandler(async(req , res)=>{
+const getUserChannelDetails = asyncHandler(async (req, res) => {
+  const { username } = req.params;
 
-  const {username} = req?.params
-  
-  if(!username?.trim()){
-    throw new ApiError(400 , "username is missing")
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing");
   }
 
- const channel = await User.aggregate(
-   
-   [
-
-     {
-      $match:{
-        username:username?.toLowerCase()
-      }
-     },{
-      $lookup:{
-        from:"subscriptions",
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
         localField: "_id",
-        foreignField:"channel",
-        as:"subscribers"
-
-      }
-     } ,{
-      $lookup:{
-        from:"subscriptions",
-        localField:"_id",
-        foreignField:"subscriber",
-        as:"subscribedTo"
-      }
-     },{
-      $addFields:{
-        subscriberCount:{
-          $size:"$subscribers"
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
         },
-        channelSubscribedToCount:{
-          $size:"$subscribedTo"
-        }
-        ,isSubscribed:{
-          $cond:{
-            if:{$in:[req.user?._id , "$subscribers.subscriber"]},
-            then:true,
-            else:false
-          }
-        }
-      }
-     },{
+        channelSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+      
+    },
+    {
       $project:{
-        fullName:1 , email:1 , username:1 , avatar:1 , subscriberCount:1  , isSubscribed:1 , channelSubscribedToCount:1
-      }
-     }
 
-    ]
-  )
+        fullName:1,
+        username:1,
+        subscribersCount:1,
+        channelSubscribedToCount:1,isSubscribed:1,
+        avatar:1 ,coverImage:1,
+        email:1
+
+      }
+    }
+  ]);
 
   if(!channel?.length){
-    throw new ApiError(404 , "Channel does not exists")
+
+    throw new ApiError(404 , "channel does not exists")
   }
-  console.log(channel , "After the Aggregation")
+  return res.status(200 , new ApiResponse(200 , channel[0] ,  "User channel fetched successFully"))
   
-
-  return res
-  .status(200)
-  .json(
-    new ApiResponse(200 , channel[0] , "User channel fetched successfully")
-  )
-
-})
-
+});
 
 export {
   registerUser,
@@ -417,6 +415,7 @@ export {
   changeCurrentPassword,
   getCurrentUser,
   updateAccountDetails,
-  updateUserAvatar,updateUserCoverImage,
-  getUserChannelDetails
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelDetails,
 };
